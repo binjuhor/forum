e75b2977862c554b0dbc48c28bf630b3e0c272ae
Hoang Kiem <kiemhd@outlook.com>
2023-12-09 21:52:29 +0700

    add: factory and database seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 
 class PostFactory extends Factory
 {
@@ -13,11 +13,9 @@ class PostFactory extends Factory
 	public function definition(): array
 	{
 		return [
-			'title' => $this->faker->word(),
-			'body' => $this->faker->word(),
-			'user_id' => $this->faker->word(),
-			'created_at' => Carbon::now(),
-			'updated_at' => Carbon::now(),
+			'user_id' => User::factory(),
+			'title' => str(fake()->sentence)->beforeLast('.')->title(),
+			'body' => fake()->realText(600),
 		];
 	}
 }
--- database/migrations/2023_12_09_094704_create_comments_table.php
+++ database/migrations/2023_12_09_094704_create_comments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
-            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Post::class);
             $table->longText('body');
             $table->timestamps();
         });
