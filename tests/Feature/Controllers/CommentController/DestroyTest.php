e1daef7d86a3df5e87ceb73f6703c3dbf1fc201f
Hoang Kiem <kiemhd@outlook.com>
2024-01-11 14:35:11 +0700

    feat: add test for delete comment

---
+++ tests/Feature/Controllers/CommentController/DestroyTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\Post;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\delete;
+
+it('requires authentication', function () {
+    delete(route('comments.destroy', Post::factory()->create()))
+        ->assertRedirect(route('login'));
+});
+
+it('can delete a comment', function(){
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)->delete(route('comments.destroy',  $comment));
+
+    $this->assertModelMissing($comment);
+});
+
+it('redirects to the post show page', function(){
+    $comment = Comment::factory()->create();
+
+    actingAs($comment->user)
+        ->delete(route('comments.destroy', $comment))
+        ->assertRedirect(route('posts.show', $comment->post_id));
+});
