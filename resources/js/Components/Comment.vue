510a65790227d2f118d2f2ca54af0722d50d88d1
Hoang Kiem <kiemhd@outlook.com>
2024-01-11 15:07:19 +0700

    feat: ui delete comment

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -6,17 +6,32 @@
         <div>
             <p class="mt-1 break-all">{{ comment.body }}</p>
             <span class="first-letter:uppercase block pt-1 text-xs text-gray-600">By {{ comment.user.name }} {{ relativeDate(comment.created_at)}} ago</span>
+            <div class="mt-1">
+                <form v-if="canDelete" @submit.prevent="deleteComment">
+                    <button type="submit" class="text-xs text-gray-600 hover:text-gray-900">Delete</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
     import {relativeDate} from "@/Utilities/date.js";
+    import { router, usePage } from '@inertiajs/vue3'
+    import {computed} from "vue";
 
-    defineProps({
+    const props = defineProps({
         comment: {
             type: Object,
             required: true,
         },
     })
+
+    const deleteComment = () => {
+        router.delete(route('comments.destroy', props.comment.id),{
+            preserveScroll: true,
+        })
+    }
+
+    const canDelete = computed(() => props.comment.user.id === usePage().props.auth.user?.id)
 </script>
--- tests/Feature/Controllers/CommentController/DestroyTest.php
+++ tests/Feature/Controllers/CommentController/DestroyTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\delete;
 
@@ -24,4 +25,12 @@ it('redirects to the post show page', function(){
     actingAs($comment->user)
         ->delete(route('comments.destroy', $comment))
         ->assertRedirect(route('posts.show', $comment->post_id));
+});
+
+it('pervents deleting a comment you didnt create', function () {
+    $comment = Comment::factory()->create();
+
+    actingAs(User::factory()->create())
+        ->delete(route('comments.destroy', $comment))
+        ->assertForbidden();
 });
