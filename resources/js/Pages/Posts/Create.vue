78f4cc9e84fc85fab8a60b2ae1dc6b43c77d1988
Hoang Kiem <kiemhd@outlook.com>
2024-02-17 18:30:06 +0700

    feat: creating post the frontend edition

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -1,11 +1,45 @@
-<script setup>
-
-</script>
-
 <template>
+    <AppLayout title="Create a Post">
+        <Container>
+            <h1 class="text-2xl font-bold">Create a Post</h1>
+            <form @submit.prevent="createPost">
+                <div class="mt-6">
+                    <InputLabel for="title" value="Title" class="sr-only" />
+                    <TextInput id="title" class="w-full" v-model="form.title"  placeholder="Give it a great title..."/>
+                    <InputError :message="form.errors.title" class="mt-2" />
+                </div>
+
+                <div class="mt-3">
+                    <InputLabel for="body" value="Body" class="sr-only" />
+                    <TextArea id="body" v-model="form.body" rows="25" />
+                    <InputError :message="form.errors.body" class="mt-2" />
+                </div>
+
+                <div class="flex justify-end mt-6">
+                    <SecondaryButton href="{{ route('posts.index') }}" class="mr-2">Cancel</SecondaryButton>
+                    <PrimaryButton type="submit">Create Post</PrimaryButton>
+                </div>
+            </form>
+        </Container>
+    </AppLayout>
 
 </template>
 
-<style scoped>
+<script setup>
+import {useForm} from '@inertiajs/vue3'
+import AppLayout from '@/Layouts/AppLayout.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import InputError from '@/Components/InputError.vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+import TextInput from '@/Components/TextInput.vue'
+import TextArea from '@/Components/TextArea.vue'
+import Container from '@/Components/Container.vue'
 
-</style>
+const form = useForm({
+    title: '',
+    body: '',
+})
+
+const createPost = () => form.post(route('posts.store'))
+</script>
---
+++ tests/Feature/Controllers/PostController/CreateTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+it('requires authentication', function() {
+    get(route('posts.create'))->assertRedirect(route('login'));
+});
+
+it('store a post', function () {
+    actingAs(User::factory()->create())
+        ->get(route('posts.create'))
+        ->assertComponent('Posts/Create');
+});
