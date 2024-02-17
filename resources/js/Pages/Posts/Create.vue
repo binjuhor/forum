f20ef87e6d1740166af0dea6efe04ce6d9abd7fd
Hoang Kiem <kiemhd@outlook.com>
2024-02-17 22:57:08 +0700

    chore: clean

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -12,7 +12,6 @@
                 <div class="mt-3">
                     <InputLabel for="body" value="Body" class="sr-only" />
                     <MarkdownEditor v-model="form.body" />
-                    <TextArea id="body" v-model="form.body" rows="25" class="mt-2" />
                     <InputError :message="form.errors.body" class="mt-2" />
                 </div>
 
