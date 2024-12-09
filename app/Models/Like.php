2f78ba34e02984a05e809a73c5e400fb641bc3a0
Hoang Kiem <kiemhd@outlook.com>
2024-12-09 22:42:37 +0700

    chore: update fucntion return type

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Like extends Model
 {
     use HasFactory;
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function likeable()
+    public function likeable(): MorphTo
     {
         return $this->morphTo();
     }
