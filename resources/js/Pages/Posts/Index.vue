fdfc6cbf94e825829f0ed5dc1d71b6ac7d57e0f6
Hoang Kiem <kiemhd@outlook.com>
2024-12-12 22:34:56 +0700

    feat: combining search with topics

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -8,14 +8,14 @@
         <menu class="flex space-x-1 mt-3 overflow-x-auto pb-2 pt-1">
           <li>
             <Pill
-              :href="route('posts.index')"
+              :href="route('posts.index', { query: searchForm.query})"
               :filled="!selectedTopic"
             >All Posts
             </Pill>
           </li>
           <li v-for="topic in topics" :key="topic.id">
             <Pill
-              :href="route('posts.index', { topic: topic.slug })"
+              :href="route('posts.index', { topic: topic.slug, query: searchForm.query })"
               :filled="topic.id === selectedTopic?.id"
             >
               {{ topic.name }}
@@ -29,6 +29,7 @@
               <div class="flex space-x-2 mt-1">
                 <TextInput v-model="searchForm.query" id="query" placeholder="Search posts..." class="w-full"/>
                 <SecondaryButton type="submit">Search</SecondaryButton>
+                <DangerButton v-if="searchForm.query" @click="clearSearch">Clear</DangerButton>
               </div>
 
             </div>
@@ -57,13 +58,14 @@
 import AppLayout from '@/Layouts/AppLayout.vue'
 import Container from '@/Components/Container.vue'
 import Pagination from '@/Components/Pagination.vue'
-import { Link, useForm } from '@inertiajs/vue3'
+import { Link, useForm, usePage } from '@inertiajs/vue3'
 import { relativeDate } from '@/Utilities/date.js'
 import PageHeading from '@/Components/PageHeading.vue'
 import Pill from '@/Components/Pill.vue'
 import InputLabel from '@/Components/InputLabel.vue'
 import TextInput from '@/Components/TextInput.vue'
 import SecondaryButton from '@/Components/SecondaryButton.vue'
+import DangerButton from '@/Components/DangerButton.vue'
 
 const props = defineProps({
   posts: {
@@ -88,7 +90,14 @@ const formattedDate = (post) => relativeDate(post.created_at)
 
 const searchForm = useForm({
   query: props.query,
+  page: 1
 })
+const page = usePage()
+const search = () => searchForm.get(page.url)
 
-const search = () => searchForm.get(route('posts.index'))
+const clearSearch = () => {
+  searchForm.query = ''
+  search()
+}
+
 </script>
